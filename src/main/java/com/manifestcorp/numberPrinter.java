198a970b56b483e04216044125b0c807023a7855
kevinjweichel@gmail.com <kevinjweichel@gmail.com>
2017-02-01 11:17:36 -0500

    initial commit of src files

---
+++ src/main/java/com/manifestcorp/numberPrinter.java
@@ -0,0 +1,21 @@
+package com.manifestcorp;
+
+class NumberPrinter{
+	
+	public static void main(String[] args){
+		System.out.println("Number printing started");
+		
+		int n;
+		
+		try{
+			n = Integer.parseInt(args[0]);
+		}catch (Exception e){
+			return;
+		}
+			
+		for(int i = 0; i <= n; i++){
+			System.out.println(i);
+		}
+	}
+
+}
---
+++ src/test/java/com/manifestcorp/CollectionTest.java
@@ -0,0 +1,44 @@
+package com.manifestcorp;
+import static org.junit.Assert.*;
+import java.util.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CollectionTest{
+	
+	private List<Integer> numberList;
+	
+	@Before
+	public void before(){
+		numberList = new ArrayList();
+	}
+	
+	@After
+	public void after(){
+		numberList = null;
+	}
+	
+	@Test
+	public void testDefaultListSize(){
+		assertEquals("Default size of list should be 0.", 0, (int)numberList.size());
+	}
+	
+	private void fillList(int n){
+		for(int i = 1; i <= n; i++){
+			numberList.add(i);
+		}
+	}
+	
+	@Test
+	public void testSecondIndexFromCollectionOfFive(){
+		fillList(5);
+		assertEquals("Second item from list", 2, (int)numberList.get(1));
+	}
+	
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void testGetting100thItemFrom5ItemCollection(){
+		numberList.get(99);
+	}
+}
