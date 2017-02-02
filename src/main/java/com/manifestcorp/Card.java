4bf1d4d89c828e62092d5947b464dbf75ccba1e8
kevinjweichel@gmail.com <kevinjweichel@gmail.com>
2017-02-02 12:08:49 -0500

    added constructor with variables to Card

--- src/main/java/com/manifestcorp/Card.java
+++ src/main/java/com/manifestcorp/Card.java
@@ -1,5 +1,10 @@
 package com.manifestcorp;
 
 public class Card {
-
+	char cardFace;
+	char cardNumber;
+	public Card(char cardFace, char cardNumber){
+		this.cardFace = cardFace;
+		this.cardNumber = cardNumber;
+	}
 }
