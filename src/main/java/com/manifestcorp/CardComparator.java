60fd0c27e8471eb5a05491986077933c6c362e25
kevinjweichel@gmail.com <kevinjweichel@gmail.com>
2017-02-02 11:31:46 -0500

    initial commit of CardComparator.java

---
+++ src/main/java/com/manifestcorp/CardComparator.java
@@ -0,0 +1,20 @@
+package com.manifestcorp;
+
+public class CardComparator {
+	char[] sortedCardValues;
+	public CardComparator(){
+		sortedCardValues = new char[]{'2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'};
+	}
+	
+	public boolean isGreaterCard(char cardGreater, char cardLess){
+		return false;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
