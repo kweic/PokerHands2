848df63844a8749843a7ca888988940561b4ef4b
kevinjweichel@gmail.com <kevinjweichel@gmail.com>
2017-02-02 12:18:43 -0500

    created cards Parser to break input into cards

---
+++ src/test/java/com/manifestcorp/CardsInputParser.java
@@ -0,0 +1,12 @@
+package com.manifestcorp;
+
+import java.util.ArrayList;
+
+public class CardsInputParser {
+
+	public ArrayList<Card> makeCardArray(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
