65a23fd10932676f1581684c38164737cac341e4
kevinjweichel@gmail.com <kevinjweichel@gmail.com>
2017-02-02 13:11:59 -0500

    functionality for card input parser

--- src/main/java/com/manifestcorp/Card.java
+++ src/main/java/com/manifestcorp/Card.java
@@ -3,7 +3,7 @@ package com.manifestcorp;
 public class Card {
 	char cardFace;
 	char cardNumber;
-	public Card(char cardFace, char cardNumber){
+	public Card(char cardNumber, char cardFace){
 		this.cardFace = cardFace;
 		this.cardNumber = cardNumber;
 	}
--- src/test/java/com/manifestcorp/CardsInputParser.java
+++ src/test/java/com/manifestcorp/CardsInputParser.java
@@ -3,10 +3,19 @@ package com.manifestcorp;
 import java.util.ArrayList;
 
 public class CardsInputParser {
-
+	
 	public ArrayList<Card> makeCardArray(String string) {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Card> cards = new ArrayList<Card>();
+		String[] cards_str_arr = string.split(" ");
+		
+		for(String card: cards_str_arr){
+			cards.add(makeCard(card));
+		}
+		return cards;
+	}
+	
+	private Card makeCard(String card){
+		return new Card(card.charAt(0), card.charAt(1));
 	}
 
 }
