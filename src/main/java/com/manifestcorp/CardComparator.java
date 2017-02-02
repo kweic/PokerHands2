c86787b0b70c5a3ac57dc72b7dc75a23a5defde8
kevinjweichel@gmail.com <kevinjweichel@gmail.com>
2017-02-02 11:46:23 -0500

    implemented isEqualCards in Comparator

--- src/main/java/com/manifestcorp/CardComparator.java
+++ src/main/java/com/manifestcorp/CardComparator.java
@@ -1,13 +1,21 @@
 package com.manifestcorp;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class CardComparator {
-	char[] sortedCardValues;
+	ArrayList<Character> sortedCardValues;
 	public CardComparator(){
-		sortedCardValues = new char[]{'2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'};
+		sortedCardValues = new ArrayList(Arrays.asList('2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'));
 	}
 	
 	public boolean isGreaterCard(char cardGreater, char cardLess){
-		return false;
+		return sortedCardValues.indexOf(cardGreater) > sortedCardValues.indexOf(cardLess);
+	}
+
+	public boolean isEqualCards(char card1, char card2) {
+		// TODO Auto-generated method stub
+		return card1 == card2;
 	}
 	
 	
