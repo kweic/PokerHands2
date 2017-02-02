d78a5b9009e50b73d70ed50395b0d04ac96be73f
kevinjweichel@gmail.com <kevinjweichel@gmail.com>
2017-02-02 14:54:14 -0500

    initial commit of FizzBuzz with UnitTests

---
+++ src/main/java/com/manifestcorp/FizzBuzzer.java
@@ -0,0 +1,23 @@
+package com.manifestcorp;
+
+
+public class FizzBuzzer {
+	
+	static String printFizzBuzzToNumber(int n){
+		StringBuilder sb = new StringBuilder();
+		for(int i = 1; i <= n; i++){
+			sb.append(fizzBuzzForDivisibleNumbers(3,5,i));
+			if(i!=n)sb.append("\n");
+		}
+		return sb.toString();
+	}
+	
+	static String fizzBuzzForDivisibleNumbers(int fizz, int buzz, int i) {
+		if(i == 0)return "0";
+		if(i % (fizz*buzz) == 0)return "FizzBuzz";
+		if(i % fizz == 0)return "Fizz";
+		if(i % buzz == 0)return "Buzz";
+		return i+"";
+	}
+
+}
