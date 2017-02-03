3e7619ad7a36403f4beaa63993f22c0e767ee1c7
kevinjweichel@gmail.com <kevinjweichel@gmail.com>
2017-02-03 14:59:30 -0500

    PokerHands restart, tests with no asserts

--- src/test/java/com/manifestcorp/PokerHandsTest.java
+++ src/test/java/com/manifestcorp/PokerHandsTest.java
@@ -2,110 +2,92 @@ package com.manifestcorp;
 
 import static org.junit.Assert.*;
 
-import java.util.ArrayList;
-
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 public class PokerHandsTest {
 
-
-	private Hand cardHand1;
-	private char KING = 'K';
-	private  char TWO = '2';
-	private HandChecker handChecker;
-	private String CARDS1 = "2H 3D 5S 9C KD";
-	private String BAD_INPUT_TOO_MANY_CARDS = CARDS1+" 4H";
-	private String BAD_INPUT_TOO_FEW_CARDS = "2H 3D 5S 9C";
-	private String CARDS_PAIR = "9C 3D 5S 9C KD";
-	private String CARDS_STRAIGHTFLUSH = "2H 3H 4H 5H 6H";
-	private String CARDS_STRAIGHT = "3D 4D 5D 6D 7D";
-	private String CARDS_FLUSH = "2H 3H 5H 8H AH";
+	private PokerHandComparator pokerHandComparator;
+	private String CARDS_FLUSH;
+	private String CARDS_STRAIGHT;
 	
-	@Before public void initialize() {
-	       handChecker = new HandChecker();
-	}
-	
-	@After
-	public void after(){
-		handChecker = null;
-	}
-
-	//tests to write
-	//card is higher than other
 	@Test
-	public void testCardIsGreater() throws Exception {
-		//iterate through cards and check all of them
-		assertTrue(handChecker.isGreaterCard(KING, TWO));
+	public void testIsFlush() {
+		fail("Not yet implemented");
 	}
 	
 	@Test
-	public void testCardIsLess() throws Exception {
-		assertFalse(handChecker.isGreaterCard(TWO, KING));
-	}
-	//card is equal to other
-	@Test
-	public void testCardsAreEqual() throws Exception {
-		assertTrue(handChecker.isEqualCards(KING, KING));
+	public void testIsStraight() {
+		fail("Not yet implemented");
 	}
 	
 	@Test
-	public void testHandCreatesCards() throws Exception {
-		cardHand1 = new Hand(CARDS1); //black
-		assertEquals("char value of card number should be equal", CARDS1.charAt(0), cardHand1.cards.get(0).cardNumber); 
-	}
-	
-	@Test(expected = IllegalArgumentException.class)
-	public void testHandThrowsExceptionIfBadInput_TooManyCards() throws Exception {
-		cardHand1 = new Hand(BAD_INPUT_TOO_MANY_CARDS);
-	}
-	
-	@Test(expected = IllegalArgumentException.class)
-	public void testHandThrowsExceptionIfBadInput_TooFewCards() throws Exception {
-		cardHand1 = new Hand(BAD_INPUT_TOO_FEW_CARDS);
-	}
-	
-	@Test(expected = IllegalArgumentException.class)
-	public void testHandThrowsExceptionIfBadInput_NullInput() throws Exception {
-		cardHand1 = new Hand(null);
-	}
-	
-	@Test(expected = IllegalArgumentException.class)
-	public void testHandThrowsExceptionIfBadInput_EmptyInput() throws Exception {
-		cardHand1 = new Hand("");
-	}
-	
-	//hand is a pair
-	@Test
-	public void testHandHasAPair() throws Exception {
-		cardHand1 = new Hand(CARDS_PAIR);
-		assertTrue(handChecker.isPair(cardHand1));
+	public void testIsStraightFlush() {
+		fail("Not yet implemented");
 	}
 	
 	@Test
-	public void testHandIsStraightFlush() throws Exception{
-		cardHand1 = new Hand(CARDS_STRAIGHTFLUSH);
-		assertTrue(handChecker.isStraightFlush(cardHand1));
+	public void testFourOfAKind() {
+		fail("Not yet implemented");
 	}
 	
 	@Test
-	public void testHandisStraight() throws Exception{
-		cardHand1 = new Hand(CARDS_STRAIGHT);
-		assertTrue(handChecker.isStraight(cardHand1));
+	public void testIsFullHouse() {
+		fail("Not yet implemented");
 	}
 	
 	@Test
-	public void testHandisFlush() throws Exception{
-		//same suit
-		cardHand1 = new Hand(CARDS_FLUSH);
-		assertTrue(handChecker.isFlush(cardHand1));
+	public void testIsThreeOfAKind() {
+		fail("Not yet implemented");
 	}
-	//hand is two pair
-	//hand is three of a kind
-	//hand is a straight
-	//hand is a flush
-	//input is valid for hands
 	
+	@Test
+	public void testIsPair() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testHighCardIsKing(){
+		fail("not implemented");
+	}
+	
+	@Test
+	public void test3isHigherThan2(){
+		fail("not implemented");
+	}
+	
+	@Test
+	public void testHandIsSorted(){
+		fail("not implemented");
+	}
+	
+	@Test
+	public void testStraightFlushBeatsFourOfAKind(){
+		fail("not implemented");
+	}
+	
+	@Test
+	public void testHandWithHighCardWins(){
+		fail("not implemented");
+	}
+	
+	@Test
+	public void testHandsTie(){
+		fail("not implemented");
+	}
+	
+	@Test
+	public void testTwoPairTieHighWins(){
+		fail("not implemented");
+	}
+	
+	@Test
+	public void testThreeOfAKindHighTripletWins(){
+		fail("not implemented");
+	}
+	
+	@Test
+	public void testBothFullHouseHigherThreeWins(){
+		fail("not implemented");
+	}
 
 }
